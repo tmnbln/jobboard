1b896120b92dea09bc3ec611898688f8d51150a3
tmnbln <smssms@wp.pl>
2024-04-18 19:40:00 +0200

    feat: working scrapper

--- server/src/controllers/puppeteer.ts
+++ server/src/controllers/puppeteer.ts
@@ -1,20 +1,29 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { executablePath } from 'puppeteer';
 
 export async function scrapeJobOffer(url: string) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch( {headless: true, executablePath: executablePath()} );
     const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'networkidle2'});
+    await page.goto(url, { waitUntil: 'networkidle2' });
 
     puppeteer.use(StealthPlugin());
 
     const jobData = await page.evaluate(() => {
-        const companyElement = document.querySelector('li.job-ad-display-ve6qfw.at-listing__list-icons-company-name');
-        const titleElement = document.querySelector('span.job-ad-display-15gph7y');
-        const locationElement = document.querySelector('span.job-ad-display-1whr5zf');
-        const descriptionElement = document.querySelector('article.job-ad-display-1lx1y7n');
-        const requirementsElement = document.querySelector('article.job-ad-display-1lx1y7n');
-        const benefitsElement = document.querySelector('article.job-ad-display-1lx1y7n');
+        // const companyElement = document.querySelector('li.job-ad-display-ve6qfw.at-listing__list-icons-company-name');
+        // const titleElement = document.querySelector('span.job-ad-display-15gph7y');
+        // const locationElement = document.querySelector('span.job-ad-display-1whr5zf');
+        // const descriptionElement = document.querySelector('article.job-ad-display-1lx1y7n');
+        // const requirementsElement = document.querySelector('article.job-ad-display-1lx1y7n');
+        // const benefitsElement = document.querySelector('article.job-ad-display-1lx1y7n');
+        // const salaryElement = document.querySelector('.salary');
+        // const url = window.location.href;
+        const companyElement = document.querySelector('a.ci__link');
+        const titleElement = document.querySelector('h1.title');
+        const locationElement = document.querySelector('a.bsj-tag.tag--location');
+        const descriptionElement = document.querySelector('div.bsj-template__content');
+        const requirementsElement = document.querySelector('div.bsj-template__content');
+        const benefitsElement = document.querySelector('div.bsj-tags');
         const salaryElement = document.querySelector('.salary');
         const url = window.location.href;
 
@@ -36,4 +45,4 @@ export async function scrapeJobOffer(url: string) {
 
 export default { scrapeJobOffer };
 
-// https://www.youtube.com/watch?v=9zwyfrVv3hg
+// https://berlinstartupjobs.com/engineering/search-ks-platform-lead-orb-software-worldcoin/
