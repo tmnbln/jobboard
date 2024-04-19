58312e29b6999146bceecd973e7c340b9fa88a39
tmnbln <smssms@wp.pl>
2024-04-19 15:52:46 +0200

    feat: clean scrapped data with regex

--- server/src/controllers/puppeteer.ts
+++ server/src/controllers/puppeteer.ts
@@ -5,29 +5,27 @@ import { executablePath } from 'puppeteer';
 puppeteer.use(StealthPlugin());
 
 export async function scrapeJobOffer(url: string) {
-    const browser = await puppeteer.launch( {headless: true, executablePath: executablePath()} );
+    const browser = await puppeteer.launch({ headless: true, executablePath: executablePath() });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
 
-
     const jobData = await page.evaluate(() => {
+        const regexSalary = /[\d,]+(?:\.?\d+)?\s*(k|K|\$|€|GBP|USD|EUR)?/;
+        const regexCleanText = /\s\s+/g;
+
         const companyElement = document.querySelector('a.ci__link');
         const titleElement = document.querySelector('h1.title');
         const locationElement = document.querySelector('a.bsj-tag.tag--location');
         const descriptionElement = document.querySelector('div.bsj-template__content');
-        const requirementsElement = document.querySelector('div.bsj-template__content');
-        const benefitsElement = document.querySelector('div.bsj-tags');
         const salaryElement = document.querySelector('.salary');
         const url = window.location.href;
 
         return {
-            company: companyElement ? companyElement.textContent : null,
-            title: titleElement ? titleElement.textContent : null,
-            location: locationElement ? locationElement.textContent : null,
-            description: descriptionElement ? descriptionElement.textContent : null,
-            requirements: requirementsElement ? requirementsElement.textContent : null,
-            benefits: benefitsElement ? benefitsElement.textContent : null,
-            salary: salaryElement ? salaryElement.textContent : null,
+            company: companyElement ? companyElement.textContent.trim().replace(regexCleanText, ' ') : null,
+            title: titleElement ? titleElement.textContent.trim().replace(regexCleanText, ' ') : null,
+            location: locationElement ? locationElement.textContent.trim().replace(regexCleanText, ' ') : null,
+            description: descriptionElement ? descriptionElement.textContent.trim().replace(regexCleanText, ' ') : null,
+            salary: salaryElement && regexSalary.test(salaryElement.textContent) ? salaryElement.textContent.match(regexSalary)[0] : null,
             url: url
         };
     });
@@ -37,5 +35,3 @@ export async function scrapeJobOffer(url: string) {
 }
 
 export default { scrapeJobOffer };
-
-// https://berlinstartupjobs.com/engineering/search-ks-platform-lead-orb-software-worldcoin/
